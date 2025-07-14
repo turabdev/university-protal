25b1340e69eff531b4d0cb2f60b81a771786c88e
turabdev <turabzaidi1971@gmail.com>
2025-07-14 20:49:50 +0500

    refactor: replace layout.js and page.js with layout.jsx and page.jsx for improved structure and styling

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './gloable.css'; // Ensure this path is correct
-export default function Home() {
-  return (
-    <div className="text-3xl text-center p-10 text-blue-500">
-      Welcome to Zaidi Sama's University Portal 👑📚
-    </div>
-  );
-}
---
+++ src/app/page.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./gloable.css"; // Ensure this path is correct
+import Image from "next/image";
+export default function Home() {
+  return (
+    // this just a test page for checking the tailwind css
+    <section className="grid grid-cols-3 grid-rows-5 gap-6 p-2 max-h-max w-full bg-black">
+      <img
+        className="border-2 border-gray-300 rounded-lg  shadow-blue-600 shadow-inner bg-white"
+        src="\lady with binocular.svg"
+        alt=""
+      />
+      <img
+        className="border-2 border-gray-300 rounded-lg  shadow-blue-600 shadow-inner bg-white"
+        src="\lady with binocular.svg"
+        alt=""
+      />
+      <img
+        className="border-2 border-gray-300 rounded-lg  shadow-blue-600 shadow-inner bg-white"
+        src="\lady with binocular.svg"
+        alt=""
+      />
+      <img
+        className="border-2 border-gray-300 rounded-lg  shadow-blue-600 shadow-inner bg-white"
+        src="\lady with binocular.svg"
+        alt=""
+      />
+      <img
+        className="border-2 border-gray-300 rounded-lg  shadow-blue-600 shadow-inner bg-white"
+        src="\lady with binocular.svg"
+        alt=""
+      />
+      <img
+        className="border-2 border-gray-300 rounded-lg  shadow-blue-600 shadow-inner bg-white"
+        src="\lady with binocular.svg"
+        alt=""
+      />
+      <img
+        className="border-2 border-gray-300 rounded-lg  shadow-blue-600 shadow-inner bg-white"
+        src="\lady with binocular.svg"
+        alt=""
+      />
+      <img
+        className="border-2 border-gray-300 rounded-lg  shadow-blue-600 shadow-inner bg-white"
+        src="\lady with binocular.svg"
+        alt=""
+      />
+      <img
+        className="border-2 border-gray-300 rounded-lg  shadow-blue-600 shadow-inner bg-white"
+        src="\lady with binocular.svg"
+        alt=""
+      />
+    </section>
+  );
+}
