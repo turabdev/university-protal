31bab9c02b168fd52740d4cd7c2c2bbd7eaadf3b
turabdev <turabzaidi1971@gmail.com>
2025-07-15 14:15:22 +0500

    fix: adjust padding in Home component for better layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     // this just a test page for checking the tailwind css
-    <section className="grid grid-cols-3 grid-rows-5 gap-6 p-2 max-h-max w-full bg-black">
+    <section className="grid grid-cols-3 grid-rows-5 gap-6 p-[50px] max-h-max w-full bg-black">
       <img
         className="border-2 border-gray-300 rounded-lg  shadow-blue-600 shadow-inner bg-white"
         src="\lady with binocular.svg"
