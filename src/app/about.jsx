81aec7ff5731697372db020f1b224b4c99de7971
turabdev <turabzaidi1971@gmail.com>
2025-07-15 14:07:54 +0500

    made a page name as ABOUT.jsx

---
+++ src/app/about.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+import "./gloable.css"; // Ensure this path is correct
+
+export default function About() {
+  return (
+    <nav>
+      <ul>
+        <li>home</li>
+        <li>vreea</li>
+      </ul>
+    </nav>
+  );
+}
